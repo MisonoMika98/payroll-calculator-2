5df818ac7fc2d507bfff4e9d4f829f03f9a7d53e
ariel raymundo <ariraymundo98@gmail.com>
2026-04-21 14:24:16 -0700

    added user input and text file creating

--- src/main/java/org/pluralsight/Main.java
+++ src/main/java/org/pluralsight/Main.java
@@ -2,20 +2,50 @@ package org.pluralsight;
 
 
 import java.io.BufferedReader;
+import java.io.FileOutputStream;
 import java.io.FileReader;
+import java.io.PrintStream;
+import java.util.Scanner;
 
 public class Main {
 
-    static void main() {
-        try {
 
-            FileReader fileReader = new FileReader("employees.csv");
-            BufferedReader bufferedReader = new BufferedReader(fileReader);
+    static void main() {
+
+        // calls the variables to assign null in order to close the file at the end
+        BufferedReader bufferedReader = null;
+        PrintStream printer = null;
+
+        Scanner userInput = new Scanner(System.in);
+
+        // stores the user's input/file name
+        System.out.print("Enter the name of the file employee file to process: ");
+        String fileName = userInput.nextLine().toLowerCase().trim();
+
+        System.out.print("Enter the name of the [payroll] file to create: ");
+        String fileNameChosen = userInput.nextLine().toLowerCase().trim();
+
+        try {
+            // variables to read the file, NOTE: fileName string in FileReader()
+            // instead of hardcoding an actual file name to let user input
+            FileReader fileReader = new FileReader(fileName);
+            bufferedReader = new BufferedReader(fileReader);
+
+            // variables to write the file, NOTE: fileNameChosen string in FileOutputStream()
+            // instead of hardcoding the file name you chose beforehand, lets you input one
+
+            FileOutputStream fileOutputStream = new FileOutputStream(fileNameChosen);
+            printer = new PrintStream(fileOutputStream);
+
 
             String lines = bufferedReader.readLine();
 
-            // skip the first line - and read again
+            // skip the first line/original header - and reads again
             lines = bufferedReader.readLine();
+
+            // new header for the file
+            printer.printf("%-10s %-25s %-15s%n", "ID", "Name", "Gross Pay");
+            printer.printf("%-10s %-25s %-15s%n", "----------", "-------------------------", "---------------");
 
             while(lines != null)
             {
@@ -27,31 +57,42 @@ public class Main {
                 double payRate = Double.parseDouble(columns[3]);
 
                 // create employee object
-                Employee employee = new  Employee(id, name, hoursWorked, payRate);
+                Employee employee = new Employee(id, name, hoursWorked, payRate);
 
-                // replace employee.getId() == int with whichever employee id in the .csv you want to print
-                // too lazy to do a scanner search tbh sorry :3
-
-                if (employee.getId() == 30) {
-                    System.out.println(employee.getName() + ", Employee ID #" + employee.getId() + "'s gross pay is $"
-                            + employee.getGrossPay());
+                // prints to file
+                if(fileName.equals("employees.csv")) {
+                    printer.printf("%-10s %-25s %-15.2f%n", employee.getId(), employee.getName(),
+                            employee.getGrossPay());
                 }
+
 
                 // breaks the endless loop
                 lines = bufferedReader.readLine();
             }
 
-            // close the file
-            bufferedReader.close();
-
+        }
+        catch (Exception ex)
+        {
+            System.out.println("What are you doing!");
+            System.out.println(ex.getMessage());
         }
 
-        catch (Exception ex){
-            System.out.println("what are you doing!");
-        }
 
+        finally
+        {
+            // 3) closes the file
+            try
+            {
+                if(bufferedReader != null) bufferedReader.close();
+                if(printer != null) printer.close();
+            }
+
+            catch(Exception ex){
+                System.out.println(ex.getMessage());
+            }
+        }
+        userInput.close();
     }
-
 
 }
 
